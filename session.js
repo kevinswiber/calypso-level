52775de27b3504f1814ce0699788b78af2e0cef6
Kevin Swiber <kswiber@gmail.com>
2014-07-31 16:29:40 -0400

    Cleaning up old code.

--- session.js
+++ session.js
@@ -5,38 +5,6 @@ var LevelSession = module.exports = function(collectionMap, cache) {
   this.cache = cache || {};
 };
 
-function convertToModel(config, entity, isBare) {
-  return entity;
-  /*var obj;
-  if (isBare) {
-    obj = entity;
-  } else {
-    obj = Object.create(config.constructor.prototype);
-    var keys = Object.keys(config.fieldMap || {});
-    keys.forEach(function(key) {
-      var prop = config.fieldMap[key] || key;
-      if (key.indexOf('.') === -1) {
-        obj[key] = entity[prop];
-      } else {
-        var parts = prop.split('.');
-        var part = parts.reduce(function(prev, cur) {
-          if (Array.isArray(prev)) {
-            return prev.map(function(item) {
-              return item[cur];
-            }); 
-          } else if (prev.hasOwnProperty(cur)) {
-            return prev[cur];
-          }
-        }, entity)
-
-        obj[key] = part;
-      }
-    });
-  }
-
-  return obj;*/
-}
-
 LevelSession.prototype.find = function(query, cb) {
   if (typeof query === 'function') {
     cb = query;
@@ -63,11 +31,8 @@ LevelSession.prototype.find = function(query, cb) {
     })
     .on('end', function() {
       var sorted = compiled.sort(buffer);
-      var entities = sorted.map(function(value) {
-        return convertToModel(query.modelConfig, value, query.modelConfig.isBare);
-      });
 
-      cb(null, entities);
+      cb(null, sorted);
     })
     .on('error', function(err) {
       cb(err);
@@ -86,8 +51,7 @@ LevelSession.prototype.get = function(query, id, cb) {
       return;
     }
 
-    var model = convertToModel(config, value, config.isBare);
-    cb(null, model);
+    cb(null, value);
   });
 };
 
